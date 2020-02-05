f03b89cc9a100beb5dfd228c58e7b0075b5a9b07
xinqiwei119 <60654971+xinqiwei119@users.noreply.github.com>
2020-02-05 22:38:28 +0800

    Initial commit

---
+++ Module02/src/abc.java
@@ -0,0 +1,7 @@
+/**
+ * @author qiwei
+ * @create 2020/2/3 - 7:07 上午
+ */
+public class abc {
+
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        char a = 'A';
+        char zh = '中';
+        System.out.println(a);
+        System.out.println(zh);
+
+
+
+
+    }
+}
